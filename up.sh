fbbf718bf714a1b1564c1c22dac659cd2aecc2a5
Lennart Wachowiak <lennart.wachowiak@univie.ac.at>
2022-02-22 15:50:10 +0000

    added reload flag to the development up.sh
    so we do not have to comment/uncomment it the whole time...

--- up.sh
+++ up.sh
@@ -6,12 +6,17 @@ then
     # Completely re-build all images from scatch without using build cache
     docker-compose build --no-cache
     docker-compose up --force-recreate -d
-else
+elif [[ $1 = reload* ]];
+then
     echo "development"
     export BUILD_ENV=development
     # Uncomment the following line if there has been an updated to overcooked-ai code
-    # docker-compose build --no-cache
-
+    docker-compose build --no-cache
+    # Force re-build of all images but allow use of build cache if possible
+    docker-compose up --build
+else
+    echo "development"
+    export BUILD_ENV=development
     # Force re-build of all images but allow use of build cache if possible
     docker-compose up --build
 fi
