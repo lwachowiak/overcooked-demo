e01194c22352ccb346ca931a1a958719cfd13674
Ptisni <peter.tisnikar@gmail.com>
2022-02-28 13:21:05 +0000

    Update up.sh to remove ffmpeg

--- up.sh
+++ up.sh
@@ -5,7 +5,7 @@ then
     export BUILD_ENV=production
     # Completely re-build all images from scatch without using build cache
     docker-compose build --no-cache
-    docker-compose up --force-recreate -d & ffmpeg -video_size $(xdpyinfo | grep 'dimensions:'|awk '{print $2}') -f x11grab -r 25 -i :0.0+0,0 -qscale 0 $(date +%Y%d%m_%H%M%S).mpg &> /dev/null && fg
+    docker-compose up --force-recreate -d
 
 # reload the overcooked-ai repository
 elif [[ $1 = re* ]];
@@ -15,20 +15,12 @@ then
     # reload everything
     docker-compose build --no-cache
     # Force re-build of all images but allow use of build cache if possible
-    docker-compose up --build & ffmpeg -video_size $(xdpyinfo | grep 'dimensions:'|awk '{print $2}') -f x11grab -r 25 -i :0.0+0,0 -qscale 0 $(date +%Y%d%m_%H%M%S).mpg &> /dev/null && fg
-
-# flag to disable screenrecoring
-elif [[ $1 = nov* ]];
-then
-    echo "development"
-    export BUILD_ENV=development
-    # Force re-build of all images but allow use of build cache if possible
-    docker-compose up --build 
+    docker-compose up --build
 
 # development build with logging in terminal+screenrecording
 else   
     echo "development standard"
     export BUILD_ENV=development
     # Force re-build of all images but allow use of build cache if possible
-    docker-compose up --build & ffmpeg -video_size $(xdpyinfo | grep 'dimensions:'|awk '{print $2}') -f x11grab -r 25 -i :0.0+0,0 -qscale 0 $(date +%Y%d%m_%H%M%S).mpg &> /dev/null && fg
+    docker-compose up --build
 fi
